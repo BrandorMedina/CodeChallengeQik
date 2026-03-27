7713abeafe79225fd397c9935df570e0985af193
Brandor Medina <brandormedina16@gmail.com>
2026-03-27 16:12:28 -0400

    ci fixed?

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,9 +3,11 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { DataSource } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
+  let dataSource: DataSource;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -14,6 +16,8 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    dataSource = app.get(DataSource);
   });
 
   it('/health (GET)', async () => {
@@ -25,6 +29,10 @@ describe('AppController (e2e)', () => {
   });
 
   afterAll(async () => {
+    if (dataSource && dataSource.isInitialized) {
+      await dataSource.destroy();
+    }
+
     await app.close();
   });
 });
